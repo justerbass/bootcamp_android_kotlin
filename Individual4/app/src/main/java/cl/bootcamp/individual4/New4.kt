05ae96e44e8fcf8437c506a31f696c53b40044d0
Javier Castillo <jkttzars@gmail.com>
2024-08-27 19:50:51 -0400

    end new excercise 4

--- Individual4/app/src/main/java/cl/bootcamp/individual4/New4.kt
+++ Individual4/app/src/main/java/cl/bootcamp/individual4/New4.kt
@@ -9,7 +9,9 @@ fun new(){
         fun showProfile() {
             println("nombre: $nombre")
             println("edad: $edad")
-            println("trabajo: $trabajo")
+            if (trabajo != null) {
+                println("trabajo: $trabajo")
+            }
             if (refencia != null) {
                 println("fue citado por: ${refencia.nombre} de ${refencia.edad} años")
                 println("")
@@ -20,9 +22,43 @@ fun new(){
     }
 
     val Walter = usuario("Walter White", 60, "Profesor", null)
-    var Jesse = usuario("Jesse Pinkman", 27, "Estudiante", Walter)
+    val Jesse = usuario("Jesse Pinkman", 27, "Estudiante", Walter)
 
-    Walter.showProfile()
-    Jesse.showProfile()
+
+    class userList (val users : MutableList<usuario> = mutableListOf()){
+        fun addUser(user : usuario){
+            users.add(user)
+        }
+        fun removeUser(user : usuario) {
+            users.remove(user)
+        }
+        fun showUsers(){
+            var i = 0
+            for (user in users){
+                i++
+                println(i)
+                user.showProfile()
+            }
+        }
+    }
+
+    val Rick = usuario("Rick Grimes", 45, "Sheriff", Walter)
+    val Shane = usuario("Shane Walsh", 43, null, Rick)
+    val Saul = usuario("Saul Goodman", 40, "Abogado", null)
+
+    val personajes : userList = userList()
+
+    personajes.addUser(Rick)
+    personajes.addUser(Shane)
+    personajes.addUser(Saul)
+    personajes.addUser(Jesse)
+    personajes.addUser(Walter)
+
+    personajes.showUsers()
+
+    personajes.removeUser(Saul)
+
+    personajes.showUsers()
+
 
 }

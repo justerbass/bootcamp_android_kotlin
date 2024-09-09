9f5e671d838fd87ff11f085ce8fcb0a1d5dbe5fb
Javier Castillo <jkttzars@gmail.com>
2024-09-09 12:20:46 -0300

    update ex 9

--- Individual9/app/src/main/java/cl/bootcamp/individual9/component/InputNumberField.kt
+++ Individual9/app/src/main/java/cl/bootcamp/individual9/component/InputNumberField.kt
@@ -3,16 +3,22 @@ package cl.bootcamp.individual9.component
 
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.text.input.KeyboardType
 
 
 @Composable
 fun NumberField(name: String){
-
-    OutlinedTextField(value = name,
-        onValueChange = {},
+    var inNumVal by remember { mutableStateOf("") }
+    OutlinedTextField(value = inNumVal,
+        onValueChange = {newVal -> inNumVal = newVal},
         keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number ),
-        singleLine = true
+        singleLine = true,
+        placeholder = {Text(text = name)}
         )
 }
